012f31066bebda4a7ca3fb3ac8f7df06a6b652a1
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-11-28 10:23:23 -0300

    feat-Controllers

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class TodoController : ControllerBase
+    {
+        
+    }
+}
